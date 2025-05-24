db43bfbeca974e70bae9ad5cba132f52ae4e536a
MisaJC <123918601+JCMisa@users.noreply.github.com>
2025-05-24 09:06:20 +0000

    show the recent session and user own companions in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,34 @@
 import CompanionCard from "@/components/custom/CompanionCard";
 import CompanionsList from "@/components/custom/CompanionsList";
 import CTA from "@/components/custom/CTA";
-import { recentSessions } from "@/constants";
+import {
+  getAllCompanions,
+  getRecentSessions,
+} from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
-export default function Home() {
+export default async function Home() {
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
     <main>
       <h1>Popular Companions</h1>
 
       <section className="home-section">
-        <CompanionCard
-          id="123"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#ffda6e"
-          bookmarked={true}
-        />
-        <CompanionCard
-          id="456"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#d62a6e"
-          bookmarked={false}
-        />
-        <CompanionCard
-          id="789"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#e5d0ff"
-          bookmarked={false}
-        />
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
       </section>
 
       <section className="home-section">
         <CompanionsList
           title="Recently Completed Sessions"
-          companions={recentSessions}
+          companions={recentSessionsCompanions}
           classNames="w-2/3 max-lg:w-full"
         />
         <CTA />
