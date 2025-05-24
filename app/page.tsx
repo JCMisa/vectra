3688bcc0ab6c7481355dd15be538491248e83189
MisaJC <123918601+JCMisa@users.noreply.github.com>
2025-05-24 05:48:48 +0000

    create the new companion form and the mockup dashboard ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,51 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/custom/CompanionCard";
+import CompanionsList from "@/components/custom/CompanionsList";
+import CTA from "@/components/custom/CTA";
+import { recentSessions } from "@/constants";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-2xl underline">Hello World</h1>
-      <Button>Click Me</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+          bookmarked={true}
+        />
+        <CompanionCard
+          id="456"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#d62a6e"
+          bookmarked={false}
+        />
+        <CompanionCard
+          id="789"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#e5d0ff"
+          bookmarked={false}
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently Completed Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   );
 }
