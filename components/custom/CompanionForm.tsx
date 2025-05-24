ee6a63926634b4ccfed1e0f516646e509dfb57a6
MisaJC <123918601+JCMisa@users.noreply.github.com>
2025-05-24 07:00:32 +0000

    integrate supabase for backend and create a new companion record

--- components/custom/CompanionForm.tsx
+++ components/custom/CompanionForm.tsx
@@ -23,8 +23,8 @@ import {
 } from "@/components/ui/select";
 import { subjects } from "@/constants";
 import { Textarea } from "@/components/ui/textarea";
-// import { createCompanion } from "@/lib/actions/companion.actions";
 import { redirect } from "next/navigation";
+import { createCompanion } from "@/lib/actions/companion.actions";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Companion is required." }),
@@ -49,14 +49,13 @@ const CompanionForm = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    // const companion = await createCompanion(values);
-    // if(companion) {
-    //     redirect(`/companions/${companion.id}`);
-    // } else {
-    //     console.log('Failed to create a companion');
-    //     redirect('/');
-    // }
-    console.log(values);
+    const companion = await createCompanion(values);
+    if (companion) {
+      redirect(`/companions/${companion.id}`);
+    } else {
+      console.log("Failed to create a companion");
+      redirect("/");
+    }
   };
 
   return (
